ad35c890acd60300351c9dc5c7963c5eef610c62
Brandon Daniel <brandanimb@gmail.com>
2020-04-28 09:54:52 -0600

    commit

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,1 +1,15 @@
 <?php
+class Core {
+    protected $currentController = "pages";
+    protected $currentMethod = "index";
+    protected $params = [];
+
+    public function __construct()
+    {
+        $url = $this->getURL();
+    }
+
+    public function getURL() {
+        echo $_GET["url"];
+    }
+}
