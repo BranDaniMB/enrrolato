a3e34df86fd7b9c7637985e134367b3377afd8be
Brandon Daniel <brandanimb@gmail.com>
2020-04-29 01:50:46 -0600

    commit

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="x-ua-compatible" content="id=edge">
     <title><?php echo SITE_NAME ?></title>
+    <!-- Scripts -->
+    <script src="/enrrolato/public/js/offline.min.js"></script>
+    <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="/enrrolato/public/css/main.css">
+    <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="/enrrolato/public/images/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/enrrolato/public/images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/enrrolato/public/images/favicon-16x16.png">
