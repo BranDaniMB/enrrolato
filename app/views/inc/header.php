31ba8c396b1dfe7dc69554c644584818ccee5f05
BranDaniMB <brandanimb@gmail.com>
2020-05-29 11:03:34 -0600

    commit

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,19 +6,19 @@
     <meta http-equiv="x-ua-compatible" content="id=edge">
     <title><?php echo $data["TITLE"] ?></title>
     <!-- Scripts -->
-    <script src="/enrrolato/public/js/offline.min.js"></script>
-    <script src="/enrrolato/public/js/main.js"></script>
+    <script src="/public/js/offline.min.js"></script>
+    <script src="/public/js/main.js"></script>
     <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="/enrrolato/public/css/main.css">
+    <link rel="stylesheet" type="text/css" href="/public/css/main.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/enrrolato/public/images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/enrrolato/public/images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/enrrolato/public/images/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/public/images/favicon-16x16.png">
 </head>
 <?php
 
-/*if ($_SESSION["ACCESS"] == AVAIL_CONNECT && !isset($_SESSION["isValidLogin"])) {
+if ($_SESSION["ACCESS"] == AVAIL_CONNECT && !isset($_SESSION["isValidLogin"])) {
     header('Location: /authentication/login/');
 } elseif ($_SESSION["ACCESS"] == AVAIL_DISCONNECT && isset($_SESSION["isValidLogin"])) {
     header('Location: /');
@@ -30,6 +30,6 @@
         <a href="/enrrolato/authentication/logout/" id="user-profile-logout">Cerrar sesión</a>
     </div>
     <?php
-}*/
+}
 ?>
 <body>
