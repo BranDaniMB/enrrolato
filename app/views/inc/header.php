529c0884dad3feabee61a60182cc9cd3e5dcb6d4
BranDaniMB <brandanimb@gmail.com>
2020-05-29 11:48:17 -0600

    Update header.php

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -8,6 +8,11 @@
     <!-- Scripts -->
     <script src="/js/offline.min.js"></script>
     <script src="/js/main.js"></script>
+    <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js"></script>
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+         https://firebase.google.com/docs/web/setup#available-libraries -->
+    <script src="https://www.gstatic.com/firebasejs/7.14.6/firebase-analytics.js"></script>
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="/css/main.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -33,3 +38,19 @@ if ($_SESSION["ACCESS"] == AVAIL_CONNECT && !isset($_SESSION["isValidLogin"])) {
 }
 ?>
 <body>
+<script>
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDoK3cxz1ssywX3BPXUOnqn1ZEb0w0K2N8",
+        authDomain: "enrrolato-1588267227733.firebaseapp.com",
+        databaseURL: "https://enrrolato-1588267227733.firebaseio.com",
+        projectId: "enrrolato-1588267227733",
+        storageBucket: "enrrolato-1588267227733.appspot.com",
+        messagingSenderId: "55283551393",
+        appId: "1:55283551393:web:0ce7cda5eb6005a1f5b4e0",
+        measurementId: "G-4Z0H0V2VXK"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+</script>
