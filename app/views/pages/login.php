2ae10fac32b3117af57c569fe7eb37b84aca06f8
BranDaniMB <brandanimb@gmail.com>
2020-05-29 11:10:55 -0600

    Update login.php

--- app/views/pages/login.php
+++ app/views/pages/login.php
@@ -9,7 +9,7 @@ $googleClient = new Google_Client();
 $auth = new Account($googleClient);
 
 if (isset($_SESSION["isValidLogin"])) {
-    header('Location: /enrrolato/');
+    header('Location: /');
 }
 
 if (isset($_GET['code'])) {
@@ -22,21 +22,21 @@ if (isset($_GET['code'])) {
     if ($auth->authenticate($_SESSION["payload"]["sub"], $_SESSION["payload"]["email"]) == 1) {
         unset($_SESSION["access_token"]);
         $_SESSION["isValidLogin"] = true;
-        header('Location: /enrrolato/');
+        header('Location: /');
     } else {
         unset($_SESSION["access_token"]);
         unset($_SESSION["token_data"]);
         $_SESSION["ERROR_TITLE"] = "Cuenta no autorizada";
         $_SESSION["ERROR_MESSAGE"] = "La cuenta de correo <u>" . $_SESSION["payload"]["email"] . "</u> no está autorizada a ingresar al sistema.";
         unset($_SESSION["payload"]);
-        header('Location: /enrrolato/systemerror');
+        header('Location: /systemerror');
     }
 }
 ?>
 <a class="google-login" href="<?php echo $auth->getAuthUrl() ?>">
-    <img alt="Cambiar imagen" onmouseout="this.src='/enrrolato/public/images/btn_google_signin_dark_normal_web@2x.png';"
-         onmouseover="this.src='/enrrolato/public/images/btn_google_signin_dark_focus_web@2x.png';"
-         src="/enrrolato/public/images/btn_google_signin_dark_normal_web@2x.png"/>
+    <img alt="Cambiar imagen" onmouseout="this.src='/images/btn_google_signin_dark_normal_web@2x.png';"
+         onmouseover="this.src='/images/btn_google_signin_dark_focus_web@2x.png';"
+         src="/images/btn_google_signin_dark_normal_web@2x.png"/>
 </a>
 
 <?php
