8022cc6f2e184ab075837dedd53f1b8ad25541e7
BranDaniMB <brandanimb@gmail.com>
2020-06-14 13:29:17 -0600

    commit

--- app/models/IngredientsModel.php
+++ app/models/IngredientsModel.php
@@ -2,7 +2,6 @@
 
 class IngredientsModel extends Base
 {
-    private $boxes = "";
     public function __construct()
     {
         parent::__construct();
@@ -37,62 +36,77 @@ class IngredientsModel extends Base
 
     public function createFlavorsBox() {
         $flavorList = $this->getInfo('business/ingredients/flavors');
-
-
-        foreach ($flavorList as &$item) {
-            $this->boxes .='<div class="ingredient-container">' .
-                        '<p class="ingredient-name">' . ucfirst($item["name"]) . '</p>' .
-                        '<p>¿Es licor?: <u>' . $this->definedIsValue($item["isLiqueur"]) . '</u></p>' .
-                        '<p>¿Sabor especial?: <u>' . $this->definedIsValue($item["isSpecial"]) . '</u></p>' .
-                        '<p>¿Es exclusivo?: <u>' . $this->definedIsValue($item["isExclusive"]) . '</u></p>' .
-                        '<p>¿Esta disponible?: <u>' . $this->definedIsValue($item["avaliable"]) . '</u></p>' .
-                        '<a class="ingredient-edit" href="/ingredients/edit/'. $item["name"] .'"><i class="material-icons">create</i></a>' .
+        if (!empty($flavorList)) {
+            $boxes = "";
+            foreach ($flavorList as &$item) {
+                $boxes .='<div class="ingredient-container">' .
+                    '<p class="ingredient-name">' . ucfirst($item["name"]) . '</p>' .
+                    '<p>¿Es licor?: <u>' . $this->definedIsValue($item["isLiqueur"]) . '</u></p>' .
+                    '<p>¿Sabor especial?: <u>' . $this->definedIsValue($item["isSpecial"]) . '</u></p>' .
+                    '<p>¿Es exclusivo?: <u>' . $this->definedIsValue($item["isExclusive"]) . '</u></p>' .
+                    '<p>¿Esta disponible?: <u>' . $this->definedIsValue($item["avaliable"]) . '</u></p>' .
+                    '<a class="ingredient-edit" href="/ingredients/edit/'. $item["name"] .'"><i class="material-icons">create</i></a>' .
                     '</div>';
-        }
+            }
 
-        return $this->boxes;
+            return $boxes;
+        } else {
+            return "No hay sabores para mostrar.";
+        }
     }
 
     public function createFillingBox() {
         $fillingList = $this->getInfo('business/ingredients/fillings');
+        if (!empty($fillingList)) {
+            $boxes = "";
+            foreach ($fillingList as &$item) {
+                $boxes .='<div class="ingredient-container">' .
+                    '<p class="ingredient-name">' . ucfirst($item["name"]) . '</p>' .
+                    '<p>¿Es exclusivo?: <u>' . $this->definedIsValue($item["isExclusive"]) . '</u></p>' .
+                    '<p>¿Esta disponible?: <u>' . $this->definedIsValue($item["avaliable"]) . '</u></p>' .
+                    '<a class="ingredient-edit" href="/ingredients/edit/'. $item["name"] .'"><i class="material-icons">create</i></a>' .
+                    '</div>';
+            }
 
-        foreach ($fillingList as &$item) {
-            $this->boxes .='<div class="ingredient-container">' .
-                '<p class="ingredient-name">' . ucfirst($item["name"]) . '</p>' .
-                '<p>¿Es exclusivo?: <u>' . $this->definedIsValue($item["isExclusive"]) . '</u></p>' .
-                '<p>¿Esta disponible?: <u>' . $this->definedIsValue($item["avaliable"]) . '</u></p>' .
-                '<a class="ingredient-edit" href="/ingredients/edit/'. $item["name"] .'"><i class="material-icons">create</i></a>' .
-                '</div>';
+            return $boxes;
+        } else {
+            return "No hay rellenos para mostrar.";
         }
-
-        return $this->boxes;
     }
 
     public function createToppingBox() {
         $toppingList = $this->getInfo('business/ingredients/toppings');
+        if (!empty($toppingList)) {
+            $boxes = "";
+            foreach ($toppingList as &$item) {
+                $boxes .='<div class="ingredient-container">' .
+                    '<p class="ingredient-name">' . ucfirst($item["name"]) . '</p>' .
+                    '<p>¿Esta disponible?: <u>' . $this->definedIsValue($item["avaliable"]) . '</u></p>' .
+                    '<a class="ingredient-edit" href="/ingredients/edit/'. $item["name"] .'"><i class="material-icons">create</i></a>' .
+                    '</div>';
+            }
 
-        foreach ($toppingList as &$item) {
-            $this->boxes .='<div class="ingredient-container">' .
-                '<p class="ingredient-name">' . ucfirst($item["name"]) . '</p>' .
-                '<p>¿Esta disponible?: <u>' . $this->definedIsValue($item["avaliable"]) . '</u></p>' .
-                '<a class="ingredient-edit" href="/ingredients/edit/'. $item["name"] .'"><i class="material-icons">create</i></a>' .
-                '</div>';
+            return $boxes;
+        } else {
+            return "No hay toppings para mostrar.";
         }
-
-        return $this->boxes;
     }
 
     public function createContainerBox() {
         $containerList = $this->getInfo('business/ingredients/containers');
+        if (!empty($containerList)) {
+            $boxes = "";
+            foreach ($containerList as &$item) {
+                $boxes .='<div class="ingredient-container">' .
+                    '<p class="ingredient-name">' . ucfirst($item["name"]) . '</p>' .
+                    '<p>¿Esta disponible?: <u>' . $this->definedIsValue($item["avaliable"]) . '</u></p>' .
+                    '<a class="ingredient-edit" href="/ingredients/edit/'. $item["name"] .'"><i class="material-icons">create</i></a>' .
+                    '</div>';
+            }
 
-        foreach ($containerList as &$item) {
-            $this->boxes .='<div class="ingredient-container">' .
-                '<p class="ingredient-name">' . ucfirst($item["name"]) . '</p>' .
-                '<p>¿Esta disponible?: <u>' . $this->definedIsValue($item["avaliable"]) . '</u></p>' .
-                '<a class="ingredient-edit" href="/ingredients/edit/'. $item["name"] .'"><i class="material-icons">create</i></a>' .
-                '</div>';
+            return $boxes;
+        } else {
+            return "No hay envases para mostrar.";
         }
-
-        return $this->boxes;
     }
 }
