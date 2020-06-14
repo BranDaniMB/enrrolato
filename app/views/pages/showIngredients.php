0c0097904c81bb1e93f92ace9c21affd4aba8829
BranDaniMB <brandanimb@gmail.com>
2020-06-13 18:03:30 -0600

    commit

--- app/views/pages/showIngredients.php
+++ app/views/pages/showIngredients.php
@@ -8,17 +8,17 @@ include VIEWS_PATH . "inc/header.php";
 <a id="add-ingredient-button" class="add-ingredient-button button-left" href="/ingredients/add">Añadir un ingrediente</a>
 
 <div class="tab">
-    <button class="tablinks" onclick="openCity(event, 'sabores')" id="defaultOpen">Sabores</button>
-    <button class="tablinks" onclick="openCity(event, 'jarabes')">Jarabes</button>
-    <button class="tablinks" onclick="openCity(event, 'topping')">Topping's</button>
-    <button class="tablinks" onclick="openCity(event, 'envases')">envases</button>
+    <button class="tablinks" onclick="openTab(event, 'flavor')" id="defaultOpen">Sabores</button>
+    <button class="tablinks" onclick="openTab(event, 'filling')">Jarabes</button>
+    <button class="tablinks" onclick="openTab(event, 'topping')">Topping's</button>
+    <button class="tablinks" onclick="openTab(event, 'container')">envases</button>
 </div>
 <?php
 $modify = new FlavorsModel();
 ?>
 
 <!-- Tab content -->
-<div id="sabores" name="flavor" class="tabcontent">
+<div id="flavor" class="tabcontent">
     <div class="ingredients-container">
     <?php
     echo $modify->createFlavorsBox();
@@ -26,15 +26,15 @@ $modify = new FlavorsModel();
     </div>
 </div>
 
-<div id="jarabes" name="filling" class="tabcontent">
+<div id="filling" class="tabcontent">
     <p>Aquí los rellenos</p>
 </div>
 
-<div id="topping" name="topping" class="tabcontent">
+<div id="topping" class="tabcontent">
     <p>Aquí los topping's</p>
 </div>
 
-<div id="envases" name="container" class="tabcontent">
+<div id="container" class="tabcontent">
     <p>Aquí los envases's</p>
 </div>
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,12 @@
-function openCity(evt, cityName) {
+function openTab(evt, type) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
     var addButton = document.getElementById("add-ingredient-button");
+    addButton.setAttribute("href", "/ingredients/add/" + type);
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        addButton.setAttribute("href", "/ingredients/add/" + tabcontent[i].getAttribute("name"));
         tabcontent[i].style.display = "none";
     }
 
@@ -17,6 +17,6 @@ function openCity(evt, cityName) {
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(type).style.display = "block";
     evt.currentTarget.className += " active";
 }
