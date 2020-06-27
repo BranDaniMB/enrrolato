697f77856811904e112ea683c615cde781bf30d1
BranDaniMB <brandanimb@gmail.com>
2020-06-26 18:20:50 -0600

    commit

--- app/views/pages/addAccount.php
+++ app/views/pages/addAccount.php
@@ -8,9 +8,9 @@ include VIEWS_PATH . "inc/header.php";
     <label>Email:
         <input id="email" class="" type="email" name="email" required></label>
     <input type="submit" value="Agregar">
+    <small>La cuenta de correo debe ser de Google, además una vez agregada aquí, se debe de iniciar sesión con ella en la app, para terminar el proceso de autenticación.</small>
 </form>
-<small>La cuenta de correo debe ser de Google, además una vez agregada aquí, se debe de iniciar sesión con ella en la app,
-    <br/>para terminar el proceso de autenticación.</small>
+
 <?php
 include VIEWS_PATH . "inc/footer.php";
 ?>
